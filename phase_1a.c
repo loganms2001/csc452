a76ea306d208d5e6f2431cee2bfdaed8b20a9fa8
Logan <logansandlin@arizona.edu>
2025-01-26 20:51:07 -0700

    it's alive!!

--- phase_1a.c
+++ phase_1a.c
@@ -1,1 +1,9 @@
-// library for phase 1a
+#include "phase_1a.h"
+
+int main() {
+    struct Process a;
+    phase1_init();
+    puts("compiles and runs");
+}
+
+void phase1_init(void) {}
--- phase_1a.h
+++ phase_1a.h
@@ -1,1 +1,31 @@
-// do a header here
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct Process // all subject to change
+{
+    int pid;
+    int (*main_func)(void*); // I think that's how you make a function pointer haha
+    struct Process *parent;
+    struct Process *first_child;
+    struct Process *following_sibling;
+};
+
+void phase1_init(void);
+
+int spork(char *name, int (*startFunc)(void*), void *arg, int stackSize, int priority);
+
+int join(int *status);
+
+void quit(int status);
+
+void zap(int pid);
+
+int getpid(void);
+
+void dumpProcesses(void);
+
+void blockMe();
+
+int unblockProc(int pid);
